422e826b7357b06fb315ff53b1a33a196386fe5a
awatef20 <awateftaher20@gmail.com>
2024-12-22 10:16:20 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+
+    document.querySelector("h1").style.backgroundColor='red';
+    document.querySelector("h1").style.color='yellow';
+    document.querySelector("h1").style.fontSize='50px';
+    let x=5,z=2;
+    if(x>z){
+        document.write("<br>"+"x=  "+x);
+    }else {
+        document.write("<br>"+"z=  "+z);
+    }
+    const a=[10,24,56,98,66,0];
+    for (let i=0;i<a.length;i++ ){
+        document.write("<br>"+a[i]);
+    
+    }document.write("<br>"+a[4]);
+
+Console.log("ekldjlksdh");
